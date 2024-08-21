e8b23c5cbc357b67e92258d6ef6a535a2612aa6f
LeeJaeHo-96 <towpam123@gmail.com>
2024-08-21 12:49:26 +0900

    Start

---
+++ Assets/Jumper.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    public float jumpPower;
+
+    public Rigidbody rigid;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+}
+
